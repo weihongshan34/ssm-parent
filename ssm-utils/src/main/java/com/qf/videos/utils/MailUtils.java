1edc515e587a391b1a4d8fdf33245e350002ba2e
weihongshan <13781670854@163.com>
2021-03-11 11:10:54 +0800

    Initial commit2

--- ssm-utils/src/main/java/com/qf/videos/utils/MailUtils.java
+++ ssm-utils/src/main/java/com/qf/videos/utils/MailUtils.java
@@ -87,6 +87,7 @@ public final class MailUtils {
                 "测试邮件随机生成的验证码是：" + getValidateCode(6));
         System.out.println("发送成功");
         System.out.println(6654654);
+        System.out.println(7777);
 
 }
 }
